29826ea5bb0567b92e3d3b4127855ad9f04b2c8c
MatsThijssen <mht283@nyu.edu>
2017-09-06 13:04:20 -0400

    implemented right biased riemann summation

--- riemann.py
+++ riemann.py
@@ -1,5 +1,5 @@
 # This is a python file! The '#' character indicates that the following line is a comment.
-
+import numpy as np
 # The following is an example for how to define a function in Python
 # def tells the compiler that hello_world is the name of a function
 # this implementation of hello_world takes a string as an argument,
@@ -7,9 +7,18 @@
 # hello_world() without an argument, then the compiler uses ''
 # as the default value of the argument.
 def hello_world(name=''):
-    print "hello world!"
-    print name
+    print("hello world!")
+    print(name)
     return
     
-    
-#Implement the Riemann Sum approximation for integrals.
+def hey(x):
+	return x**2
+
+#Implement the Riemann Sum approximation for integrals
+def Riemann(func, n, a, b):
+	width=(b-a)/n
+	total=0
+	for i in np.arange(a+width,b+width,width):
+		total+=func(i)*width
+	return total
+print(Riemann(hey,5,1,6))	
